6970c9b4dba78499174932ca42f63f451ca023c0
Konstantin Korolenko <softslot-com@yandex.ru>
2022-06-25 15:33:28 +0300

    refactoring

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -8,9 +8,9 @@ const gameplay = () => {
   const randomNum1 = Math.ceil(Math.random() * 100);
   const randomNum2 = Math.ceil(Math.random() * 100);
 
-  const divisor = randomNum1 < randomNum2 ? randomNum1 : randomNum2;
+  const smallestNum = randomNum1 < randomNum2 ? randomNum1 : randomNum2;
   let correctAnswer = 1;
-  for (let i = divisor; i > 1; i -= 1) {
+  for (let divisor = smallestNum; divisor > 1; divisor -= 1) {
     if (randomNum1 % divisor === 0 && randomNum2 % divisor === 0) {
       correctAnswer = divisor;
       break;
