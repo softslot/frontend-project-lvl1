0c550e649b02eda59f324b33b233187e5a785cc3
Konstantin Korolenko <softslot-com@yandex.ru>
2022-06-25 15:18:33 +0300

    refactoring bin/brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -4,19 +4,24 @@ import play from '../src/index.js';
 
 const gameTitle = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const gameplay = () => {
-  const randomNum = Math.floor(Math.random() * 101);
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
 
-  let correctAnswer = 'yes';
-  if (randomNum > 1) {
-    for (let divisor = Math.floor(randomNum / 2); divisor > 1; divisor -= 1) {
-      if (randomNum % divisor === 0) {
-        correctAnswer = 'no';
-        break;
-      }
+  for (let divisor = 2; divisor <= num / 2; divisor += 1) {
+    if (num % divisor === 0) {
+      return false;
     }
   }
 
+  return true;
+};
+
+const gameplay = () => {
+  const randomNum = Math.floor(Math.random() * 101);
+
+  const correctAnswer = isPrime(randomNum) ? 'yes' : 'no';
   const question = randomNum;
 
   return [question, correctAnswer];
