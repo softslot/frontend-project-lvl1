71ce1f3f741580d738a90e9da9b9a008fc708615
Konstantin Korolenko <softslot-com@yandex.ru>
2022-06-25 14:52:15 +0300

    add bin/brain-progression.js

---
+++ bin/brain-progression.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import play from '../src/index.js';
+
+const gameTitle = 'What number is missing in the progression?';
+const minLengthProgression = 5;
+
+const gameplay = () => {
+  const initialProgression = Math.floor(Math.random() * 101);
+  const stepProgression = Math.ceil(Math.random() * 10);
+  const lengthProgression = Math.floor(Math.random() * 10) + minLengthProgression;
+
+  const progression = [];
+  for (let i = 0; i < lengthProgression; i += 1) {
+    progression.push(initialProgression + (stepProgression * i));
+  }
+
+  const randomKey = Math.floor(Math.random() * progression.length);
+
+  const correctAnswer = progression[randomKey];
+  progression[randomKey] = '..';
+  const question = progression.join(' ');
+
+  return [question, correctAnswer];
+};
+
+play(gameTitle, gameplay);
